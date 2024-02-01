24e06662b1f1bb0f43f1510316e37f28e3d24594
craii <dios-c@hotmail.com>
2024-02-01 16:22:20 +0800

    1
    1

--- data_storage/message_storage.py
+++ data_storage/message_storage.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.append(f"{Path(__file__).resolve().parent.parent}")
+
 from utils import date_string, date_stamp
 from data_storage.sqlite_controller import SqliteController
 
@@ -20,5 +24,5 @@ class MessageStorage(object):
         create_time = f"{date_stamp().lstrip('_')}"
         content = msg
         insert_data_query = '''INSERT INTO conversation (session, name, create_time, content)
-                                      VALUES ('{}', '{}', '{}', '{}')'''.format(session, name, create_time, content)
+                                      VALUES ("{}", "{}", "{}", "{}")'''.format(session, name, create_time, content)
         self.editor.write(conn="conversation", insert_data_query=insert_data_query)
